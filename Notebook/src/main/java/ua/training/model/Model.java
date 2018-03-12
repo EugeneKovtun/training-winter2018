37fda8b8657a80d6d26cadb5b13bf599ec2946cb
EugeneKovtun <eugene.kraros@gmail.com>
2018-03-12 15:29:38 +0200

    Created class model

--- Notebook/src/main/java/ua/training/model/Model.java
+++ Notebook/src/main/java/ua/training/model/Model.java
@@ -1,5 +1,21 @@
 package ua.training.model;
 
-public class Model {
+import ua.training.model.entities.Record;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+    private List<Record> records = new ArrayList<>();
+
+    public List<Record> getRecords() {
+        return records;
+    }
+
+    public void setRecords(List<Record> records) {
+        this.records = records;
+    }
+    public void addRecord(Record record){
+        records.add(record);
+    }
 }
